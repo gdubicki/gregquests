e7149899e2c896c12ab9aac4cfd64c888999b3f7
Greg Dubicki <gdubicki@egnyte.com>
2020-11-11 15:32:51 +0100

    Fix URL

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="Drop-in wrapper around the Python Requests library"
     " that provides extra features.",
     long_description=convert_markdown_to_rst("README.md"),
-    url="https://github.com/gdubicki/requests_extra",
+    url="https://github.com/gdubicki/requests-extra",
     author="Greg Dubicki and the upstream projects authors",
     keywords=["requests", "timeout", "retry", "exception", "brotli"],
     classifiers=[
