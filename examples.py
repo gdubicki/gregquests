cfd71e1e15a4a299970e46294dfb8c03b213ce2e
Greg Dubicki <gdubicki@egnyte.com>
2021-01-01 13:15:19 +0100

    Refactor + add logging to show that automatic sessions work

--- examples.py
+++ examples.py
@@ -2,30 +2,43 @@
 
 from requests.exceptions import RetryError, ConnectionError
 from requests_extra.api import get
+import logging
 
-import http
-# http.client.HTTPConnection.debuglevel = 1
 
-print("Example 1: brotli support")
 
-print(get("https://httpbin.org/headers"))
+print("*** Example 1: brotli support ***")
 
-print("Example 2: automatic reusing HTTP connections support")
+response = get("http://httpbin.org/headers")
+print(response.request.headers)
 
-print(get("https://httpbin.org/cookies/set/foo/bar"))
 
-print("Automatic sessions DO NOT store cookies to simulate non-sessioned requests for drop-in replacement")
 
-print(get("https://httpbin.org/cookies"))
+print("*** Example 2: automatic reusing HTTP connections support ***")
 
-print("Example 3: default retries")
+print("Enabling debug logging to show this.")
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+print("   Making 1st connection to httpbin.org over HTTPS:")
+response = get("https://httpbin.org/cookies/set/foo/bar")
+print("   Note that we are NOT storing the cookies to emulate non-session behavior for a drop-in replacement!")
+print(response.cookies)
+
+print("   Making 2nd connection to httpbin.org over HTTPS:")
+response = get("https://httpbin.org/cookies/set/what/ever")
+print(response.cookies)
+
+logging.disable(logging.DEBUG)
+
+
+
+print("*** Example 3: default retries ***")
 
 try:
     print(get("https://httpbin.org/status/429"))
 except RetryError:
     print("Max retries reached, as expected.")
 
-print("Example 4: default timeout + retries")
+print("*** Example 4: default timeout + retries ***")
 
 try:
     print(get("https://httpbin.org/delay/15"))
