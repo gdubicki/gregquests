61c1f5d630537f03409dc5a680f7f278e6193dd2
Greg Dubicki <gdubicki@egnyte.com>
2021-01-02 09:42:05 +0100

    Ignore these are they are autogenerated members

--- requests_extra/defaults.py
+++ requests_extra/defaults.py
@@ -8,12 +8,12 @@ retries_total = 2  # = 3 requests in total
 retries_backoff_factor = 1  # retry after 1, 2, 4 secs
 
 retries_status_forcelist = [
-    requests.codes.internal_server_error,  # HTTP 500
-    requests.codes.bad_gateway,  # HTTP 502
-    requests.codes.service_unavailable,  # HTTP 503
-    requests.codes.gateway_timeout,  # HTTP 504
-    requests.codes.request_timeout,  # HTTP 408
-    requests.codes.too_many_requests,  # HTTP 429
+    requests.codes.internal_server_error,  # HTTP 500 # pylint: disable=no-member
+    requests.codes.bad_gateway,  # HTTP 502 # pylint: disable=no-member
+    requests.codes.service_unavailable,  # HTTP 503 # pylint: disable=no-member
+    requests.codes.gateway_timeout,  # HTTP 504 # pylint: disable=no-member
+    requests.codes.request_timeout,  # HTTP 408 # pylint: disable=no-member
+    requests.codes.too_many_requests,  # HTTP 429 # pylint: disable=no-member
 ]
 
 retries_allowed_methods = [
